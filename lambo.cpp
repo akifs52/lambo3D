0a9cdd42db3cc5d76893f15e73b94450e44e10d3
akifs52 <akifselim02@gmail.com>
2025-08-11 09:51:14 +0300

    lambo3D Qt C++

--- lambo.cpp
+++ lambo.cpp
@@ -1,78 +1,77 @@
 #include "lambo.h"
 
-#include <Qt3DRender/QCamera>
+#include <Qt3DExtras/Qt3DWindow>
 #include <Qt3DRender/QPointLight>
 #include <Qt3DExtras/QCuboidMesh>
 #include <Qt3DExtras/QPhongMaterial>
-#include <Qt3DExtras/QForwardRenderer>
 #include <Qt3DCore/QTransform>
+#include <Qt3DExtras/QOrbitCameraController>
 #include <QHBoxLayout>
-#include <QUrl>
 #include <QDebug>
-#include <QMouseEvent>
-#include <QtMath>
 
 LamboWidget::LamboWidget(QWidget *parent)
     : QWidget(parent),
     view(new Qt3DExtras::Qt3DWindow()),
-    sceneRoot(new Qt3DCore::QEntity()),
-    roadTransform(nullptr),
-    roadTimer(new QTimer(this)),
+    viewContainer(nullptr),
+    sceneRoot(nullptr),
     carEntity(nullptr),
     sceneLoader(nullptr),
-    carAndRoadRoot(new Qt3DCore::QEntity(sceneRoot)),
-    carAndRoadTransform(new Qt3DCore::QTransform()),
-    mousePressed(false),
-    azimuth(0.0f),
-    elevation(20.0f)
+    camera(nullptr),
+    roadTransform(nullptr),
+    roadTimer(new QTimer(this))
+
 {
     view->defaultFrameGraph()->setClearColor(QColor("#1c2331"));
 
     viewContainer = QWidget::createWindowContainer(view, this);
-    viewContainer->setMinimumSize(400, 300);
+    viewContainer->setMinimumSize(480, 320);
     viewContainer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-    // Burada event filter ekliyoruz
-    viewContainer->installEventFilter(this);
 
     auto *lay = new QHBoxLayout(this);
     lay->setContentsMargins(0,0,0,0);
     lay->addWidget(viewContainer);
 
+    // Root entity
+    sceneRoot = new Qt3DCore::QEntity();
+
+    // Kamera ve ışık ayarları
     setupCamera();
     setupLight(sceneRoot);
 
-    carAndRoadRoot->addComponent(carAndRoadTransform);
+    // Sahneyi oluştur
+    createScene();
 
-    createRoad(carAndRoadRoot);
-    loadCarModel(carAndRoadRoot);
-
+    // Root entity'i görüntüye bağla
     view->setRootEntity(sceneRoot);
 
+    // Kamera kontrolcüyü kur (mouse ile orbit, pan ve zoom)
+    auto *camController = new Qt3DExtras::QOrbitCameraController(sceneRoot);
+    camController->setCamera(camera);
+    camController->setLinearSpeed(50.0f);
+    camController->setLookSpeed(180.0f);
+
+    // Yol animasyonu timer
     connect(roadTimer, &QTimer::timeout, this, &LamboWidget::updateRoadAnimation);
     roadTimer->start(16);
 }
 
+LamboWidget::~LamboWidget()
+{
+    if (roadTimer) roadTimer->stop();
+
+}
+
 void LamboWidget::setupCamera()
 {
-    auto *camera = view->camera();
+    camera = view->camera();
     camera->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-
-    float distance = 12.0f;
-    float radAzimuth = qDegreesToRadians(azimuth);
-    float radElevation = qDegreesToRadians(elevation);
-
-    float x = distance * cos(radElevation) * sin(radAzimuth);
-    float y = distance * sin(radElevation);
-    float z = distance * cos(radElevation) * cos(radAzimuth);
-
-    camera->setPosition(QVector3D(x, y, z));
+    camera->setPosition(QVector3D(0.0f, 5.0f, 12.0f));
     camera->setViewCenter(QVector3D(0.0f, 0.5f, 0.0f));
 }
 
 void LamboWidget::setupLight(Qt3DCore::QEntity *parent)
 {
-    Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity(parent);
+    auto *lightEntity = new Qt3DCore::QEntity(parent);
     auto *light = new Qt3DRender::QPointLight(lightEntity);
     light->setColor("white");
     light->setIntensity(1.0f);
@@ -86,8 +85,7 @@ void LamboWidget::setupLight(Qt3DCore::QEntity *parent)
 
 void LamboWidget::createRoad(Qt3DCore::QEntity *parent)
 {
-    Qt3DCore::QEntity *roadEntity = new Qt3DCore::QEntity(parent);
-
+    auto *roadEntity = new Qt3DCore::QEntity(parent);
     auto *roadMesh = new Qt3DExtras::QCuboidMesh();
     roadMesh->setXExtent(10.0f);
     roadMesh->setYExtent(0.1f);
@@ -97,13 +95,14 @@ void LamboWidget::createRoad(Qt3DCore::QEntity *parent)
     roadTransform->setTranslation(QVector3D(0.0f, -0.05f, 0.0f));
 
     auto *roadMaterial = new Qt3DExtras::QPhongMaterial();
-    roadMaterial->setDiffuse(QColor(50,50,50));
+    roadMaterial->setDiffuse(QColor(50, 50, 50));
 
     roadEntity->addComponent(roadMesh);
     roadEntity->addComponent(roadTransform);
     roadEntity->addComponent(roadMaterial);
 
-    Qt3DCore::QEntity *lineEntity = new Qt3DCore::QEntity(parent);
+    // Orta çizgi
+    auto *lineEntity = new Qt3DCore::QEntity(parent);
     auto *lineMesh = new Qt3DExtras::QCuboidMesh();
     lineMesh->setXExtent(0.3f);
     lineMesh->setYExtent(0.06f);
@@ -123,96 +122,48 @@ void LamboWidget::createRoad(Qt3DCore::QEntity *parent)
 void LamboWidget::loadCarModel(Qt3DCore::QEntity *parent)
 {
     carEntity = new Qt3DCore::QEntity(parent);
-
     sceneLoader = new Qt3DRender::QSceneLoader(carEntity);
+
     connect(sceneLoader, &Qt3DRender::QSceneLoader::statusChanged,
             this, &LamboWidget::onSceneStatusChanged);
 
     QUrl modelUrl("qrc:/sources/sources/lamborghini.obj");
     sceneLoader->setSource(modelUrl);
 
+    carEntity->addComponent(sceneLoader);
+
     auto *carTransform = new Qt3DCore::QTransform();
     carTransform->setScale(1.0f);
     carTransform->setTranslation(QVector3D(0.0f, 0.0f, 0.0f));
-
-    carEntity->addComponent(sceneLoader);
     carEntity->addComponent(carTransform);
+}
+
+void LamboWidget::createScene()
+{
+    auto *roadRoot = new Qt3DCore::QEntity(sceneRoot);
+    createRoad(roadRoot);
+
+    auto *carRoot = new Qt3DCore::QEntity(sceneRoot);
+    loadCarModel(carRoot);
 }
 
 void LamboWidget::onSceneStatusChanged(Qt3DRender::QSceneLoader::Status status)
 {
     if (status == Qt3DRender::QSceneLoader::Ready) {
-        qDebug() << "Model yüklendi.";
+        qDebug() << "[Lambo] Model yüklendi.";
     } else if (status == Qt3DRender::QSceneLoader::Loading) {
-        qDebug() << "Model yükleniyor...";
+        qDebug() << "[Lambo] Model yükleniyor...";
     } else if (status == Qt3DRender::QSceneLoader::Error) {
-        qDebug() << "Model yüklenemedi!";
+        qDebug() << "[Lambo] Model yüklenemedi! (QSceneLoader Error)";
     }
 }
 
 void LamboWidget::updateRoadAnimation()
 {
-    if (!roadTransform || !carAndRoadTransform) return;
-
+    if (!roadTransform) return;
     static float offset = 0.0f;
-    static float rotationAngle = 0.0f;
-
     offset += 0.3f;
     if (offset > 50.0f) offset = 0.0f;
     roadTransform->setTranslation(QVector3D(0.0f, -0.05f, offset));
-
-    rotationAngle += 0.3f;
-    if (rotationAngle > 360.0f) rotationAngle -= 360.0f;
-
-    carAndRoadTransform->setRotation(QQuaternion::fromEulerAngles(0.0f, rotationAngle, 0.0f));
 }
 
-bool LamboWidget::eventFilter(QObject *obj, QEvent *event)
-{
-    if (obj == viewContainer) {
-        if (event->type() == QEvent::MouseButtonPress) {
-            QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-            if (mouseEvent->button() == Qt::LeftButton) {
-                mousePressed = true;
-                lastMousePos = mouseEvent->pos();
-                return true;
-            }
-        }
-        else if (event->type() == QEvent::MouseMove) {
-            if (mousePressed) {
-                QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-                QPoint delta = mouseEvent->pos() - lastMousePos;
-                lastMousePos = mouseEvent->pos();
-
-                azimuth += delta.x() * 0.5f;
-                elevation += -delta.y() * 0.5f;
-
-                if (elevation > 89.0f) elevation = 89.0f;
-                if (elevation < -89.0f) elevation = -89.0f;
-
-                auto *camera = view->camera();
-                float distance = 12.0f;
-                float radAzimuth = qDegreesToRadians(azimuth);
-                float radElevation = qDegreesToRadians(elevation);
-
-                float x = distance * cos(radElevation) * sin(radAzimuth);
-                float y = distance * sin(radElevation);
-                float z = distance * cos(radElevation) * cos(radAzimuth);
-
-                camera->setPosition(QVector3D(x, y, z));
-                camera->setViewCenter(QVector3D(0.0f, 0.5f, 0.0f));
-
-                return true;
-            }
-        }
-        else if (event->type() == QEvent::MouseButtonRelease) {
-            QMouseEvent *mouseEvent = static_cast<QMouseEvent*>(event);
-            if (mouseEvent->button() == Qt::LeftButton) {
-                mousePressed = false;
-                return true;
-            }
-        }
-    }
-
-    return QWidget::eventFilter(obj, event);
-}
--- lambo.h
+++ lambo.h
@@ -1,56 +1,51 @@
-#ifndef LAMBOWIDGET_H
-#define LAMBOWIDGET_H
+#ifndef LAMBO_H
+#define LAMBO_H
 
 #include <QWidget>
-#include <Qt3DExtras/Qt3DWindow>
-#include <Qt3DCore/QEntity>
-#include <Qt3DCore/QTransform>
-#include <Qt3DRender/QSceneLoader>
 #include <QTimer>
-#include <QMouseEvent>   // Fare eventleri için ekle
+#include <Qt3DCore/QEntity>
+#include <Qt3DRender/QCamera>
+#include <Qt3DRender/QSceneLoader>
+#include <Qt3DExtras/Qt3DExtras>
+QT_BEGIN_NAMESPACE
+
+QT_END_NAMESPACE
 
 class LamboWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit LamboWidget(QWidget *parent = nullptr);
-
-protected:
-
-    bool eventFilter(QObject *obj, QEvent *event) override;
+    ~LamboWidget();
 
 private slots:
-    void onSceneStatusChanged(Qt3DRender::QSceneLoader::Status status);
     void updateRoadAnimation();
+    void onSceneStatusChanged(Qt3DRender::QSceneLoader::Status status);
+
+private:
     void setupCamera();
     void setupLight(Qt3DCore::QEntity *parent);
     void createRoad(Qt3DCore::QEntity *parent);
     void loadCarModel(Qt3DCore::QEntity *parent);
+    void createScene();
+
+
 
 private:
-
 
     Qt3DExtras::Qt3DWindow *view;
     QWidget *viewContainer;
     Qt3DCore::QEntity *sceneRoot;
 
-    Qt3DCore::QTransform *roadTransform;
-    QTimer *roadTimer;
-
     Qt3DCore::QEntity *carEntity;
     Qt3DRender::QSceneLoader *sceneLoader;
 
-    // Yeni: araba ve yol entitylerinin ortak root'u
-    Qt3DCore::QEntity *carAndRoadRoot;
-    Qt3DCore::QTransform *carAndRoadTransform;
+    Qt3DRender::QCamera *camera;
 
-    // Fare takibi
-    QPoint lastMousePos;
-    bool mousePressed;
+    Qt3DCore::QTransform *roadTransform;
+    QTimer *roadTimer;
 
-    // Kamera açıları
-    float azimuth = 0.0f;    // Yatay açı
-    float elevation = 20.0f; // Dikey açı, biraz yukarıya bakacak şekilde başlat
+
 };
 
-#endif // LAMBOWIDGET_H
+#endif // LAMBO_H
